f4e003beddab838a45efe27f19749193873f947f
Hylke Stapersma <hylke.stapersma@gmail.com>
2016-11-07 12:56:29 +0100

    Added swagger configuration

--- src/main/java/com/sensordatex/kafka/rest/resource/TopicResource.java
+++ src/main/java/com/sensordatex/kafka/rest/resource/TopicResource.java
@@ -2,6 +2,10 @@ package com.sensordatex.kafka.rest.resource;
 
 import com.sensordatex.kafka.rest.resource.dto.Topic;
 import com.sensordatex.kafka.rest.service.TopicService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import kafka.admin.AdminUtils;
 import kafka.admin.RackAwareMode;
 import kafka.utils.ZkUtils;
@@ -21,6 +25,7 @@ import java.util.Properties;
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 @Path("topic")
+@Api("Topic resource")
 public class TopicResource {
 
     private final TopicService topicService;
@@ -30,17 +35,32 @@ public class TopicResource {
         this.topicService = topicService;
     }
 
+    @ApiOperation(value = "Get Kafka topics")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Topics")
+    })
     @GET
     public Collection<String> getTopics() {
         return topicService.getTopics();
     }
 
+    @ApiOperation(value = "Get Kafka topic with details")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Topic details"),
+            @ApiResponse(code = 404, message = "Topic is not found")
+    })
     @GET
     @Path("/{topicName}")
     public Topic getTopic(final @PathParam("topicName") String topicName) {
         return topicService.getTopic(topicName);
     }
 
+    @ApiOperation(value = "Get Kafka topic with details")
+    @ApiResponses(value = {
+            @ApiResponse(code = 204, message = "Topic created or updated"),
+            @ApiResponse(code = 404, message = "Topic is not found"),
+            @ApiResponse(code = 400, message = "Invalid topic configuration"),
+    })
     @POST
     public void createTopic(final Topic topic) {
         topicService.createTopic(topic);
