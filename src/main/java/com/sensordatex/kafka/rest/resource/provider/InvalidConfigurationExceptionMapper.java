68770d5c59332166d0efe9969bb6cbdf78f46d7b
Hylke Stapersma <hylke.stapersma@gmail.com>
2016-12-18 20:51:23 +0100

    Added extra logging for exception

--- src/main/java/com/sensordatex/kafka/rest/resource/provider/InvalidConfigurationExceptionMapper.java
+++ src/main/java/com/sensordatex/kafka/rest/resource/provider/InvalidConfigurationExceptionMapper.java
@@ -1,5 +1,6 @@
 package com.sensordatex.kafka.rest.resource.provider;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.common.errors.InvalidConfigurationException;
 
 import javax.ws.rs.core.Response;
@@ -10,10 +11,12 @@ import javax.ws.rs.ext.Provider;
  * Created by hylke on 06/11/2016.
  */
 @Provider
+@Slf4j
 public class InvalidConfigurationExceptionMapper implements ExceptionMapper<InvalidConfigurationException> {
 
     @Override
     public Response toResponse(final InvalidConfigurationException exception) {
+        log.error("Invalid configuration", exception);
         return Response.status(Response.Status.BAD_REQUEST).build();
     }
 }
